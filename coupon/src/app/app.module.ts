82b5658eccec814638d618f7c258c8eafad6bf42
abhishekra07 <abhira719@gmail.com>
2021-08-02 16:52:40 +0530

    added product and coupon forms

--- coupon/src/app/app.module.ts
+++ coupon/src/app/app.module.ts
@@ -2,13 +2,46 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { Routes, RouterModule } from '@angular/router';
+import { AddCouponComponent } from './add-coupon/add-coupon.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'add-coupon',
+    component: AddCouponComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddCouponComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    RouterModule.forRoot(routes),
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- product/src/app/app.module.ts
+++ product/src/app/app.module.ts
@@ -5,15 +5,42 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddProductComponent } from './add-product/add-product.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'add-product',
+    component: AddProductComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    RouterModule.forRoot(routes),
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
